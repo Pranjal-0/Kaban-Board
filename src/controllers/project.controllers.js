12108b47edb346b11a6bd581a6df3790f4138371
Pranjal <pranjalgp19@gmail.com>
2026-05-16 20:56:02 +0530

    completed getProjects

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,7 +1,66 @@
-const getProjects = async (req, res) => {
-  // get all projects
-};
-
+import mongoose from "mongoose";
+import { Project } from "../models/project.models";
+import { User } from "../models/user.models";
+import { ApiError } from "../utils/api-errors";
+import { ApiResponse } from"../utils/api-response";
+import { ProjectMember } from"../models/projectmember.models.js"
+import { asyncHandler } from "../utils/async-handler";
+import {AvailableUserRoles,UserRolesEnum} from "../utils/constants.js";
+const getProjects = asyncHandler(async (req,res) => {
+  const projects =  await ProjectMember.aggregate([
+    {
+      $match:{
+        user: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup:{
+        from:"projects",
+        localField:"project",
+        foreignField:"_id",
+        as:"project",
+        pipeline:[
+          {
+            $lookup:{
+              from:"projectmembers",
+              localField:"_id",
+              foreignField:"project",
+              as:"projectmember",
+            },
+          },
+          {
+            $addFields:{
+              members:{
+                $size:"$projectmember",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind:"$project",
+    },
+    {
+      $project:{
+        Project:{
+          _id : 1,
+          name : 1,
+          description : 1,
+          members : 1,
+          createdAt : 1,
+          createdBy : 1,
+        },
+        role : 1,
+        _id : 0,  
+      },
+    },
+  ]);
+  return res
+  .status(200)
+  .json(new ApiResponse(200, projects, "Projects fetched successfully"));
+},
+)
 const getProjectById = async (req, res) => {
   // get project by id
 };
