314c1967ea07af1cf2a5ec280b36f84cf981ae95
Pranjal <pranjalgp19@gmail.com>
2026-05-24 02:00:07 +0530

    completed project controller

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -119,21 +119,155 @@ const deleteProject = asyncHandler(async (req,res) => {
     .json(new ApiResponse(200, project, "Project deleted successfully"));
 
 })
-const getProjectMembers = async (req, res) => {
-  // get project members
-};
+//ye samajha hai 
+const getProjectMembers = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+  const project = await Project.findById(projectId);
 
-const addMemberToProject = async (req, res) => {
-  // add member to project
-};
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
 
-const deleteMember = async (req, res) => {
-  // delete member from project
-};
+  const projectMembers = await ProjectMember.aggregate([
+    {
+      $match: {
+        project: new mongoose.Types.ObjectId(projectId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "user",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        user: {
+          $arrayElemAt: ["$user", 0],
+        },
+      },
+    },
+    {
+      $project: {
+        project: 1,
+        user: 1,
+        role: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        _id: 0,
+      },
+    },
+  ]);
 
-const updateMemberRole = async (req, res) => {
-  // update member role
-};
+  return res
+    .status(200)
+    .json(new ApiResponse(200, projectMembers, "Project members fetched"));
+});
+
+const addMemberToProject = asyncHandler(async (req,res) => {
+  const{email,username,role} = req.body;
+  const{projectId} = req.params;
+  const user = await user.findOne({
+    $or:[{username},{email}],
+  });
+  if(!user){
+    throw new ApiError(404, "User does not exist");
+  }
+  await ProjectMember.findOneAndUpdate(
+    {
+      user: new mongoose.Types.ObjectId(user._id),
+      project:new mongoose.Types.ObjectId(projectId),
+    },
+    {
+      user: new mongoose.Types.ObjectId(user._id),
+      project:new mongoose.Types.ObjectId(projectId),
+    },
+    {
+      new:true,
+      upsert:true,
+    },
+  );
+   return res
+    .status(201)
+    .json(new ApiResponse(201, {}, "Project member added successfully"));
+
+})
+
+const deleteMember = asyncHandler(async (req,res) => {
+  const {projectId,userId} = req.params;
+
+  let projectMember = await ProjectMember.findOne({
+    project: new mongoose.Types.ObjectId(projectId),
+    user: new mongoose.Types.ObjectId(userId)
+  });
+  let projectMember = await ProjectMember.findByIdAndDelete(projectMember._id);
+
+  if (!projectMember) {
+    throw new ApiError(404, "Project member not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        projectMember,
+        "Project member deleted successfully",
+      ),
+    );
+});
+//yesamajhna hai
+const updateMemberRole = asyncHandler(async (req, res) => {
+  const { projectId, userId } = req.params;
+  const { newRole } = req.body;
+
+  if (!AvailableUserRoles.includes(newRole)) {
+    throw new ApiError(400, "Invalid role");
+  }
+
+  let projectMember = await ProjectMember.findOne({
+    project: new mongoose.Types.ObjectId(projectId),
+    user: new mongoose.Types.ObjectId(userId),
+  });
+
+  if (!projectMember) {
+    throw new ApiError(404, "Project member not found");
+  }
+
+  projectMember = await ProjectMember.findByIdAndUpdate(
+    projectMember._id,
+    {
+      role: newRole,
+    },
+    { new: true },
+  );
+
+  if (!projectMember) {
+    throw new ApiError(404, "Project member not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        projectMember,
+        "Project member role updated successfully",
+      ),
+    );
+});
 
 export {
   addMemberToProject,
